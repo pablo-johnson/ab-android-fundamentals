0d99fac28a1b924372a8277da4965a8fe8ac83e2
Eduardo Medina <emedinaa@gmail.com>
2018-02-15 19:54:19 -0500

    *

--- UIStarted/app/src/main/java/com/androidbootcamp/androidtemplate/UIActivity.java
+++ UIStarted/app/src/main/java/com/androidbootcamp/androidtemplate/UIActivity.java
@@ -1,18 +1,20 @@
 package com.androidbootcamp.androidtemplate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class UIActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ui);
+        //setContentView(R.layout.activity_ui);
         //LinearLayout
         //setContentView(R.layout.layout_linear);
 
         //RelativeLayout
         //setContentView(R.layout.layout_relative);
+
+        setContentView(R.layout.layout_vine_sign_in);
     }
 }
