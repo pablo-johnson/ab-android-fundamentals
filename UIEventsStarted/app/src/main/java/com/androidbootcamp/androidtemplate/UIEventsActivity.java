8ffd2e326f2913f98579613de12fb331b19763f4
Eduardo Medina <emedinaa@gmail.com>
2018-02-15 19:14:39 -0500

    *

--- UIEventsStarted/app/src/main/java/com/androidbootcamp/androidtemplate/UIEventsActivity.java
+++ UIEventsStarted/app/src/main/java/com/androidbootcamp/androidtemplate/UIEventsActivity.java
@@ -1,13 +1,31 @@
 package com.androidbootcamp.androidtemplate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class UIEventsActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uievents);
+
+        /*button= findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showMessage();
+            }
+        });*/
+    }
+
+    private void showMessage(){
+        Toast.makeText(this,"Hello Android",
+                Toast.LENGTH_LONG).show();
     }
 }
