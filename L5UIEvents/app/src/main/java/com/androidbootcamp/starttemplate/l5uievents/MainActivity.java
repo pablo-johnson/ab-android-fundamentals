bb7fb74db450c3ccfdbca163bf64c6cf193df735
Pablo Johnson <pablo.88j@gmail.com>
2018-02-19 18:15:03 -0500

    L5 start project

---
+++ L5UIEvents/app/src/main/java/com/androidbootcamp/starttemplate/l5uievents/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidbootcamp.starttemplate.l5uievents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
